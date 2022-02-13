b64f4d578660bbe3596c57a4f9bc537c56df8250
Daniel Tran <daniel.tran11@gmail.com>
2022-02-13 14:12:39 +1100

    Initial setup for unit tests
    Also fixing up some of the dependencies and scripts

---
+++ src/utils.test.ts
@@ -0,0 +1,7 @@
+// import { getConfigData, processConfigData } from './utils';
+
+describe('Suite 1', () => {
+    test('adds 1 + 2 to equal 3', () => {
+        expect(1 + 2).toBe(3);
+    });
+});
